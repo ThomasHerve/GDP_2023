9c6e65fb9e8a018979fb7f325928981b4ed436db
Thomas <therve38@gmail.com>
2023-05-06 15:23:33 +0200

    Players stats

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Tagged Scene Objects
+    GameObject canvas;
+    
+    bool isPaused = true;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
+        Debug.Log(canvas);
+        canvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerStats.experience >= PlayerStats.nextLevelExp)
+        {
+            isPaused = true;
+            PlayerStats.LevelUp();
+            canvas.SetActive(true);
+        }
+
+        if (isPaused)
+        {
+            return;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerStats
+{
+    //Players levels
+    static public int experience = 0;
+    static public int level = 1;
+    static public int nextLevelExp = level * level * 10;
+
+    //Players stats
+    static public int hpMax = 10;
+    static public int hp;
+    static public int resistance = 100;
+    static public int damage = 100;
+    
+
+    static public void LevelUp()
+    {
+        level+=1;
+        experience = 0;
+    }
+
+}
