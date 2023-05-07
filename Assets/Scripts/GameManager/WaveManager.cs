b1cbee840ab3519fdcc8e0a874de2e86fbbea9ef
Thomas Herve <therve38@gmail.com>
2023-05-07 10:56:30 +0200

    Fix spawn

--- Assets/Scripts/GameManager/WaveManager.cs
+++ Assets/Scripts/GameManager/WaveManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class WaveManager : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class WaveManager : MonoBehaviour
     public float distanceSpawn;
     private GameObject player;
     public GameObject enemy;
+
+    Vector3 boxSize = new Vector3(3,3,3);
 
     // Start is called before the first frame update
     void Start()
@@ -34,13 +37,50 @@ public class WaveManager : MonoBehaviour
 
     public Vector3 GetCoordinates()
     {
+        Vector3 pos = Vector3.zero;
+        bool found = false;
+        while(!found)
+        {
+            found = true;
+            pos = CalculateCoordinates();
+
+            // CHeck coordinate is valid
+            Vector3 boxExtents = boxSize / 2f;
+            Vector3 boxMin = pos - boxExtents;
+            Vector3 boxMax = pos + boxExtents;
+
+            // Récupérer tous les colliders situés dans la boîte
+            Collider[] colliders = Physics.OverlapBox(pos, boxExtents);
+
+            // Parcourir tous les colliders récupérés
+            foreach (Collider collider in colliders)
+            {
+                if(collider.gameObject.tag != "Plane")
+                {
+                    found = false;
+                }
+            }
+            if (colliders.Length != 1)
+            {
+                found = false;
+            }
+        }
+
+        return pos;
+        // return Vector3.zero;
+    }
+
+    public Vector3 CalculateCoordinates()
+    {
+        if (player == null)
+            player = GameObject.FindGameObjectWithTag("Player");
         // Determiner où mettre un monstre par rapport au joueur
         float angle = Random.Range(0, 360);
         float r = Random.Range(25, 35);
         Vector3 playerPostion = player.transform.position;
-        float x = (playerPostion.x + r * Mathf.Cos(Mathf.Deg2Rad*angle));
-        float z = (playerPostion.z + r * Mathf.Sin(Mathf.Deg2Rad*angle));
+        float x = (playerPostion.x + r * Mathf.Cos(Mathf.Deg2Rad * angle));
+        float z = (playerPostion.z + r * Mathf.Sin(Mathf.Deg2Rad * angle));
+
         return new Vector3(x, player.transform.position.y, z);
-        // return Vector3.zero;
     }
 }
